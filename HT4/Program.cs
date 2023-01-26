b9020634d7635d488ee500b25c61e665ffebd13b
Alex Gura <aagura@yandex.ru>
2023-01-26 14:28:12 +0300

    Сделали упр4

---
+++ HT4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите a");
+string astring = Console.ReadLine ();
+int a = Convert.ToInt32(astring) ;
+int count = 1;
+while (count<=a)
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine (count);
+    }
+count++;
+}
