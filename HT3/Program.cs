6dab022dd94d701adebf1822ba601f1af3848edb
Alex Gura <aagura@yandex.ru>
2023-01-26 14:13:18 +0300

    Сделали упр3

---
+++ HT3/Program.cs
@@ -0,0 +1,19 @@
+﻿ //Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+
+Console.WriteLine("Введите число");
+string astring = Console.ReadLine ();
+double a = Convert.ToDouble (astring) ;
+
+if (a%2 == 0)
+{
+   Console.WriteLine("Да");
+}
+else 
+{
+   Console.WriteLine("Нет");
+   }
